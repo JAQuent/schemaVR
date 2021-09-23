b8e862fe9acb2973bab690135aa043a6139618f2
Jörn Alexander Quent <alexander.quent@rub.de>
2021-09-23 16:00:37 +0800

    Delete selecting_participants.R

--- paper/background_work/selecting_participants.R
+++
@@ -1,8 +0,0 @@
-exp3_sets <- ddply(dataSchemaVR3_AFC, c('subNum', 'setNum'), summarise, N = length(subNum))
-
-table(exp3_sets$setNum)
-
-
-exp4_sets <- ddply(dataSchemaVR4_AFC, c('subNum', 'setNum'), summarise, N = length(subNum))
-
-table(exp3_sets$setNum)
